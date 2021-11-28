3252f45e83f3b5ab4af7584c1c6d2c1305a740c7
complectzero <ichirkov@kse.org.ua>
2021-11-28 11:42:27 +0200

    corrections

--- for-loop.cpp
+++ for-loop.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-using namespace std;
 
-int add_nums(int nums[]) {
+int addNums(int nums[], int amount) {
     int sum = 0;
-    for(int i = 0; i < 2; i++) {
+    for(int i = 0; i < amount; i++) {
         sum += nums[i];
     }
     return sum;
 }
 
 int main() {
-    int input_nums[2] = {3, 4};
-    cout << add_nums(input_nums);
+    int inputNums[] = {3, 4};
+    int amountOfNums = sizeof(inputNums)/sizeof(*inputNums);
+    int result = addNums(inputNums, amountOfNums);
+    printf("%d", result);
     return 0;
+}
 }
